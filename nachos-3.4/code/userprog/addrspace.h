91714758e03974a593f13a93d1ae62c761793157
cajunDev <70783710+cajunDev@users.noreply.github.com>
2021-03-27 16:47:01 -0500

    Update addrspace.h for task 3

--- nachos-3.4/code/userprog/addrspace.h
+++ nachos-3.4/code/userprog/addrspace.h
@@ -31,12 +31,18 @@ class AddrSpace {
 
     void SaveState();			// Save/restore address space-specific
     void RestoreState();		// info on a context switch 
+	
+	 // method to load page
+    void loadPage(int VirtualAddr);
 
   private:
     TranslationEntry *pageTable;	// Assume linear page table translation
 					// for now!
     unsigned int numPages;		// Number of pages in the virtual 
 					// address space
+	
+	   // exe to load page
+      OpenFile *exe;
 };
 
 #endif // ADDRSPACE_H
